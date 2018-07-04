787e0fb5b7634f97edba94c677f383776c9d1b39
Blake Fischer <bfischer1121@gmail.com>
2018-07-04 17:21:00 -0400

    domain for TwitterClient

--- src/TwitterClient.js
+++ src/TwitterClient.js
@@ -2,4 +2,4 @@ const { Client } = require('./Client.js');
 
 class TwitterClient extends Client{}
 
-exports.TwitterClient = TwitterClient.getClient(TwitterClient, '206.189.186.85', 3011, 3013);
+exports.TwitterClient = TwitterClient.getClient(TwitterClient, 'cryptosecondary.com', 3011, 3013);
